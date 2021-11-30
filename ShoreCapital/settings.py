b342e2049c2b6e4fdd9512da2ec74fb16519e9f2
Khan Asfi Reza <khanasfireza10@gmail.com>
2021-11-30 16:59:28 +0600

    PostCSS

--- ShoreCapital/settings.py
+++ ShoreCapital/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django_brotli.middleware.BrotliMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
